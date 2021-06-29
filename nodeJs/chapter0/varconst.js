a10bf2a1fc1ad0097683563ca876a0a7e9927aa2
BegginerPark <78770250+BegginerPark@users.noreply.github.com>
2021-06-29 17:15:10 +0900

    Add files via upload

---
+++ nodeJs/chapter0/varconst.js
@@ -0,0 +1,15 @@
+var name = 'Austin';
+console.log(name)
+
+
+// 코드량이 많아질때 파악하기 힘들어지고 의도하지 않은
+// 변수 값으로 출력될 수가 있다.
+// 이러한 단점을 보완하려고 등장한 방식이 cons 방식이다.
+
+let name = 'Austin';
+console.log(name);
+
+// 변수 재선언이 되지 않는다.
+
+const name = 'Austin';
+console.log(name);
